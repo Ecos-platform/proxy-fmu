e191d7faba08e6c66b626e7b102f5e9279b49677
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2022-09-05 20:49:23 +0200

    no boost::process

--- src/proxyfmu/process_helper.hpp
+++ src/proxyfmu/process_helper.hpp
@@ -4,8 +4,7 @@
 
 #include <proxyfmu/fs_portability.hpp>
 
-#include <boost/dll/runtime_symbol_info.hpp>
-#include <boost/process.hpp>
+#include <subprocess/subprocess.h>
 
 #include <condition_variable>
 #include <exception>
@@ -13,8 +12,30 @@
 #include <mutex>
 #include <string>
 
+#ifdef WIN32
+#    define WIN32_LEAN_AND_MEAN
+#    include <windows.h>
+#endif
+#ifdef __linux__
+#    include <climits>
+#    include <cstring>
+#    include <unistd.h>
+#endif
+
 namespace proxyfmu
 {
+
+std::string getLoc()
+{
+#ifdef __linux__
+    char result[PATH_MAX];
+    ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
+    return std::string(result, (count > 0) ? count : 0);
+#else
+    char result[MAX_PATH];
+    return {result, GetModuleFileName(nullptr, result, MAX_PATH)};
+#endif
+}
 
 void start_process(
     const proxyfmu::filesystem::path& fmuPath,
@@ -32,18 +53,13 @@ void start_process(
 #endif
 
     if (!proxyfmu::filesystem::exists(executable)) {
-        boost::dll::fs::error_code ec;
-        boost::dll::fs::path loc = boost::dll::program_location(ec);
-        if (!ec.failed()) {
-            executable = loc.parent_path().string() / executable;
-        } else {
-            std::cerr << "[proxyfmu] Error, unable to locate parent executable" << std::endl;
-        }
+        const std::string loc = getLoc();
+        executable = proxyfmu::filesystem::path(loc).parent_path().string() / executable;
     }
 
     if (!proxyfmu::filesystem::exists(executable)) {
-        auto execPath = proxyfmu::filesystem::absolute(executable).string();
-        throw std::runtime_error("[proxyfmu] No proxyfmu executable found. " + execPath + " does not exist!");
+        const auto execPath = proxyfmu::filesystem::absolute(executable).string();
+        throw std::runtime_error("[proxyfmu] No proxyfmu executable found. '" + execPath + "' does not exist!");
     }
 
     std::string execStr = executable.string();
@@ -59,43 +75,50 @@ void start_process(
     std::cout << "\n";
     std::cout << "[proxyfmu] Booting FMU instance '" << instanceName << "'.." << std::endl;
 
-    std::string cmd(execStr + " --fmu \"" + fmuPath.string() + "\" --instanceName " + instanceName);
+    const std::string fmuPathStr = fmuPath.string();
+    std::vector<const char*> cmd = {execStr.c_str(), "--fmu", fmuPathStr.c_str(), "--instanceName", instanceName.c_str(), nullptr};
 
-    boost::process::ipstream pipe_stream;
-    boost::process::child c(cmd, boost::process::std_out > pipe_stream);
+    struct subprocess_s process
+    {
+    };
+    int result = subprocess_create(cmd.data(), subprocess_option_inherit_environment | subprocess_option_no_window, &process);
 
     bool bound = false;
-    std::string line;
-    while (pipe_stream && std::getline(pipe_stream, line)) {
-        if (!bound && line.substr(0, 16) == "[proxyfmu] port=") {
-            {
-                std::lock_guard<std::mutex> lck(mtx);
-                port = std::stoi(line.substr(16));
-                std::cout << "[proxyfmu] FMU instance '" << instanceName << "' instantiated using port " << port << std::endl;
+    if (result == 0) {
+        FILE* p_stdout = subprocess_stdout(&process);
+        char buffer[256];
+        while (fgets(buffer, 256, p_stdout)) {
+            std::string line(buffer);
+            if (!bound && line.substr(0, 16) == "[proxyfmu] port=") {
+                {
+                    std::lock_guard<std::mutex> lck(mtx);
+                    port = std::stoi(line.substr(16));
+                    std::cout << "[proxyfmu] FMU instance '" << instanceName << "' instantiated using port " << port << std::endl;
+                }
+                cv.notify_one();
+                bound = true;
+            } else if (line.substr(0, 16) == "[proxyfmu] freed") {
+                break;
+            } else {
+                std::cerr << line << std::endl;
             }
-            cv.notify_one();
-            bound = true;
-        } else if (line.substr(0, 16) == "[proxyfmu] freed") {
-            break;
+        }
+
+        int status = 0;
+        result = subprocess_join(&process, &status);
+
+        if (result == 0 && status == 0 && bound) {
+            return;
         } else {
-            std::cerr << line << std::endl;
+            std::cerr << "[proxyfmu] External proxy process for instance '"
+                      << instanceName << "' returned with status "
+                      << std::to_string(status) << ". Unable to bind.." << std::endl;
         }
     }
+    std::lock_guard<std::mutex> lck(mtx);
+    port = -999;
 
-    c.wait();
-    int status = c.exit_code();
-
-    if (status == 0 && bound) {
-        return;
-    } else {
-        std::cerr << "[proxyfmu] External proxy process for instance '"
-                  << instanceName << "' returned with status "
-                  << std::to_string(status) << ". Unable to bind.." << std::endl;
-        std::lock_guard<std::mutex> lck(mtx);
-        port = -999;
-
-        cv.notify_one();
-    }
+    cv.notify_one();
 }
 
 } // namespace proxyfmu
